83c981a4c213af29420696b276348d514e0627e0
Emil Haldan <emilhaldan@live.dk>
2024-05-16 13:45:15 +0200

    Changin file structure

---
+++ dashboard.py
@@ -0,0 +1,257 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import sys
+import os 
+import math
+from datetime import datetime, timedelta
+from data_loader import transform_coordinates, load_track_data, load_race_data
+from prepare_laps import prepare_laps_data, re_prepare_laps_data, get_specific_lap
+from data_visualizations import plot_track
+import time
+
+def update_start_time():
+    st.session_state.start_time = datetime.now()
+
+
+def download_track_data():
+    if 'left_side_df' not in st.session_state or 'right_side_df' not in st.session_state:
+        left_side_df, right_side_df = load_track_data()
+        left_side_df = transform_coordinates(left_side_df)
+        right_side_df = transform_coordinates(right_side_df)
+
+        st.session_state['left_side_df'] = left_side_df
+        st.session_state['right_side_df'] = right_side_df
+
+
+def update_global(name):
+    # if the name is different, then we need to update the track data and the name
+    if 'name' not in st.session_state or st.session_state['name'] != name:
+        st.session_state['name'] = name
+    
+    if f'laps_df_{name}' in st.session_state and f'lap_times_{name}' in st.session_state:
+        st.session_state['laps_df'] = st.session_state[f'laps_df_{name}']
+        st.session_state['lap_times'] = st.session_state[f'lap_times_{name}']
+    else: 
+        laps_df, lap_times = prepare_laps_data(name = name)
+        lap_times = lap_times.reset_index(drop=True)
+        st.session_state['laps_df'] = laps_df
+        st.session_state['lap_times'] = lap_times
+        st.session_state[f'laps_df_{name}'] = laps_df
+        st.session_state[f'lap_times_{name}'] = lap_times
+
+
+def update_global_opponent(name):
+    if 'name_opponent' not in st.session_state or st.session_state['name_opponent'] != name:
+        st.session_state['name_opponent'] = name
+    
+    if f'laps_df_{name}' in st.session_state and f'lap_times_{name}' in st.session_state:
+        st.session_state['laps_df_opponent'] = st.session_state[f'laps_df_{name}']
+        st.session_state['lap_times_opponent'] = st.session_state[f'lap_times_{name}']
+    else: 
+        laps_df, lap_times = prepare_laps_data(name = name)
+        lap_times = lap_times.reset_index(drop=True)
+        st.session_state['laps_df_opponent'] = laps_df
+        st.session_state['lap_times_opponent'] = lap_times
+        st.session_state[f'laps_df_{name}'] = laps_df
+        st.session_state[f'lap_times_{name}'] = lap_times
+
+ 
+def selected_lap_number(name):
+    lap_times = st.session_state['lap_times']
+
+    list_of_lap_options = lap_times["Lap Number"].tolist()
+    list_of_lap_times   = lap_times["Lap Time"].tolist()
+    list_of_lap_options = [str(x) + ". " + " ---> " + list_of_lap_times[idx]  for idx,x in enumerate(list_of_lap_options)]
+
+    lap_number = st.sidebar.selectbox("Select a lap number", list_of_lap_options)
+    lap_number = int(lap_number.split(".")[0]) -1
+
+    if 'lap_number' not in st.session_state or st.session_state['lap_number'] != lap_number:
+        st.session_state['lap_number'] = lap_number
+        update_track_data()
+
+
+def selected_lap_number_opponent(name):
+    lap_times = st.session_state['lap_times_opponent']
+
+    list_of_lap_options = lap_times["Lap Number"].tolist()
+    list_of_lap_times   = lap_times["Lap Time"].tolist()
+    list_of_lap_options = [str(x) + ". " + " ---> " + list_of_lap_times[idx]  for idx,x in enumerate(list_of_lap_options)]
+
+    lap_number = st.sidebar.selectbox("Select opponent lap number", list_of_lap_options)
+    lap_number = int(lap_number.split(".")[0]) -1
+
+    if 'lap_number_opponent' not in st.session_state or st.session_state['lap_number_opponent'] != lap_number:
+        st.session_state['lap_number_opponent'] = lap_number
+        update_track_data(opponent = True)
+
+
+def update_track_data(opponent = False):
+    if opponent:
+        lap_number = st.session_state['lap_number_opponent']
+        laps_df = st.session_state['laps_df_opponent']
+        specific_lap_df = get_specific_lap(laps_df, lap_number=lap_number)
+        st.session_state['cur_lap_df_opponent'] = specific_lap_df
+    else: 
+        lap_number = st.session_state['lap_number']
+        laps_df = st.session_state['laps_df']
+        specific_lap_df = get_specific_lap(laps_df, lap_number=lap_number)
+        st.session_state['cur_lap_df'] = specific_lap_df
+
+
+def page1():
+    
+    
+    lap_times = st.session_state['lap_times']
+    laps_df = st.session_state['laps_df']
+
+    opp_lap_times = st.session_state['lap_times_opponent']
+    opp_laps_df = st.session_state['laps_df_opponent']
+
+    col1, col2 = st.columns(2)
+    height = 550
+    with col1:
+        st.title(f"{st.session_state['name']}")
+        # avg_lap_time_ms
+        avg_lap_time = lap_times["Lap Time"].apply(lambda x: float(x.split(":")[0]) * 60 + float(x.split(":")[1])).mean()
+        avg_lap_time_min = str(math.floor(avg_lap_time / 60)) 
+        avg_lap_time_sec = str(round(avg_lap_time % 60, 3))
+        if len(avg_lap_time_sec.split(".")[0]) == 1:
+            avg_lap_time_sec = "0"+avg_lap_time_sec
+        avg_lap_time = avg_lap_time_min + ":" + avg_lap_time_sec
+        # Display Total Laps and Best Lap but add space between the two
+        st.markdown(f"```\nTotal Laps: {len(lap_times)}            Avg. Lap: {avg_lap_time}            Best Lap: {lap_times['Lap Time'][0]}\n```", unsafe_allow_html=True)
+        st.dataframe(lap_times, height=height, use_container_width=True)
+
+    with col2:
+        # Centered text
+        st.title(f"{st.session_state['name_opponent']}")
+        opp_avg_lap_time = opp_lap_times["Lap Time"].apply(lambda x: float(x.split(":")[0]) * 60 + float(x.split(":")[1])).mean()
+        opp_avg_lap_time_min = str(math.floor(opp_avg_lap_time / 60)) 
+        opp_avg_lap_time_sec = str(round(opp_avg_lap_time % 60, 3))
+        if len(opp_avg_lap_time_sec.split(".")[0]) == 1:
+            opp_avg_lap_time_sec = "0"+opp_avg_lap_time_sec
+        opp_avg_lap_time = opp_avg_lap_time_min + ":" + opp_avg_lap_time_sec
+        # Display Total Laps and Best Lap but add space between the two
+        st.markdown(f"```\nBest Lap: {opp_lap_times['Lap Time'][0]}            Avg. Lap: {opp_avg_lap_time}            Total Laps: {len(opp_lap_times)}\n```", unsafe_allow_html=True)
+        st.dataframe(opp_lap_times[["Lap Time", "Lap Number", "Lap Placement"]], use_container_width=True,  height=height)
+    
+    
+def page2():
+    
+    st.title("Overview of Lap")
+    # Crete an element where you can select the lap number that is not a slider 
+    left_side_df = st.session_state['left_side_df']
+    right_side_df = st.session_state['right_side_df']
+    cur_lap_df = st.session_state['cur_lap_df']
+    opp_cur_lap_df = st.session_state['cur_lap_df_opponent']
+
+    zoom = 15.15
+    # Latitude is The Y-axis (More is North (up), Less is South (down))
+    # Longitude is The X-axis (More is East (left), Less is West (right))
+    center_dict = {"Lat":50.3315, "Lon":6.941}
+    bearing = -35
+
+    plotly_fig = plot_track(cur_lap_df, opp_cur_lap_df, left_side_df, right_side_df, zoom = zoom, bearing = bearing,
+                            center_dict = center_dict , player_name = st.session_state['name'], opponent_name = st.session_state['name_opponent'])
+    st.plotly_chart(plotly_fig, use_container_width=True)
+
+
+def page3():
+
+    st.title("Corner 1 Best Laps")
+    player_name = st.session_state['name']
+    opp_name = st.session_state['name_opponent']
+
+    if opp_name == player_name:
+        st.write("Please select a different opponent")
+    else:
+        gif_path = f"Gifs/corner_animation_1_{player_name}_{opp_name}.gif"
+        st.image(gif_path, use_column_width=True)
+
+
+def page4():
+
+    st.title("Corner 2 Best Laps")
+
+    player_name = st.session_state['name']
+    opp_name = st.session_state['name_opponent']
+    if opp_name == player_name:
+        st.write("Please select a different opponent")
+    else:
+        gif_path = f"Gifs/corner_animation_2_{player_name}_{opp_name}.gif"
+        st.image(gif_path, use_column_width=True)
+
+
+def page5():
+
+    st.title("Corner 3 Best Laps")
+
+    player_name = st.session_state['name']
+    opp_name = st.session_state['name_opponent']
+    if opp_name == player_name:
+        st.write("Please select a different opponent")
+    else:
+        gif_path = f"Gifs/corner_animation_3_{player_name}_{opp_name}.gif"
+        st.image(gif_path, use_column_width=True)
+
+
+def page6():
+
+    st.title("Detailed Analysis of Player Lap")
+    # Your code for visualization goes here
+    gif_path = f"Gifs/loading.gif"
+    st.image(gif_path, use_column_width=True)
+
+
+
+# Pre-requisites
+st.set_page_config(layout="wide")
+update_start_time()
+download_track_data()
+
+players   = ['Emil','Ana','Bot']
+opponents = ['Ana','Emil','Bot']
+
+# Sidebar for Player
+st.sidebar.title('Select Player')
+name = st.sidebar.selectbox('Choose a player', players, key='name')
+update_global(name = name)
+selected_lap_number(name)
+
+# # Sidebar for Opponent
+st.sidebar.title('Select Opponent')
+name_opponent = st.sidebar.selectbox('Choose an opponent', opponents , key='name_opponent')
+update_global_opponent(name = name_opponent)
+selected_lap_number_opponent(name_opponent)
+
+
+# Dictionary of pages
+pages = {
+    "Select Dataset": page1,
+    "Whole Track": page2,
+    "Section 1"  : page3,
+    "Section 2" : page4,
+    "Section 3": page5,
+    "Detailed Analysis": page6
+}
+
+# Sidebar for navigation
+st.sidebar.title('Navigation')
+selection = st.sidebar.radio("Go to", list(pages.keys()))
+
+# for _ in range(14): 
+#     st.sidebar.write("")  # This adds some space between the stuff and the button
+
+if 'start_time' in st.session_state:
+    elapsed_time = datetime.now() - st.session_state.start_time
+    # Display the elapsed time in the sidebar
+    st.sidebar.write("")  # You can use empty writes to add space if needed
+    st.sidebar.write(f"Loading time: {elapsed_time.seconds}s {elapsed_time.microseconds // 1000}ms")
+
+st.sidebar.button("Recompute Data for {}".format(name), on_click=re_prepare_laps_data, args=([name]), help=f"Recomputes and cleans the dataset for {name}")
+
+# Display the selected page
+page = pages[selection]
+page()
---
+++ data_animation.py
@@ -0,0 +1,241 @@
+import pandas as pd
+import imageio
+import sys
+import os
+import time
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import kaleido
+from PIL import Image
+import plotly.io as pio
+from multiprocessing import Pool, cpu_count
+
+
+from data_loader import transform_coordinates, load_track_data, load_race_data, reduce_dataframe
+from prepare_laps import prepare_laps_data, get_specific_lap
+from data_visualizations import plot_track, plot_speed
+
+
+def create_single_frame(filtered_cur_df, filtered_opp_df, size, player_name, opponent_name, i,
+                        left_side_df, right_side_df, zoom, center_dict, width, height, bearing, x_time_lim):
+
+    if i/20 > filtered_cur_df['Local_Timestamp'].max() and i/20 > filtered_opp_df['Local_Timestamp'].max():
+        print(f"Frame {i} skipped because it is out of bounds")
+        return False
+
+    local_filtered_cur_df = filtered_cur_df[filtered_cur_df['Local_Timestamp'] < i/20]
+    local_filtered_opp_df = filtered_opp_df[filtered_opp_df['Local_Timestamp'] < i/20]
+
+    local_image = plot_track(local_filtered_cur_df, local_filtered_opp_df, 
+                            left_side_df, right_side_df, 
+                            zoom, center_dict, width, height, bearing, size,
+                            player_name, opponent_name, animate = True, iteration = i)
+
+    local_speed_fig = plot_speed(local_filtered_cur_df, local_filtered_opp_df, player_name, opponent_name,
+                                x_lim = x_time_lim, figsize_width = width//100, animate = True, iteration = i)
+
+    tmp_local_image = Image.open(local_image)
+    tmp_local_speed_fig = Image.open(local_speed_fig)
+    
+    new_image_width = max(tmp_local_image.width, tmp_local_speed_fig.width)
+    new_image_height = tmp_local_image.height + tmp_local_speed_fig.height
+    new_image = Image.new('RGB', (new_image_width, new_image_height))
+
+    # Paste the matplotlib figure on top
+    new_image.paste(tmp_local_speed_fig, (0, 0))
+
+    left = 0
+    upper = tmp_local_speed_fig.height
+    right = tmp_local_image.width
+    lower = upper + tmp_local_image.height
+
+    tmp_local_image = tmp_local_image.crop((0, 0, min(right, new_image_width), min(lower, new_image_height - upper)))
+    new_image.paste(tmp_local_image, (left, upper, right, lower))
+    
+    image_path = f"finished_animation_figures/frame_{i}.png"
+    new_image.save(image_path)
+
+    print(f"Finished creating frame {i}")
+    
+    return image_path
+
+
+def create_data_subset(lap_df, min_timestamp, max_timestamp, min_lat, max_lat, min_lon, max_lon, track_df = False):
+
+    if track_df:
+        conditions = (
+            (lap_df['Timestamp']  > min_timestamp) )
+    else: 
+        conditions = (
+            (lap_df['Timestamp'] > min_timestamp) &
+            (lap_df['Timestamp'] < max_timestamp) &
+            (lap_df['Longitude'] > min_lon) &
+            (lap_df['Longitude'] < max_lon) &
+            (lap_df['Latitude']  > min_lat) &
+            (lap_df['Latitude']  < max_lat)
+        )
+
+    filtered_df = lap_df[conditions]    
+    
+    local_min_timestamp = filtered_df['Timestamp'].min()
+    filtered_df['Local_Timestamp'] = filtered_df['Timestamp'] - local_min_timestamp
+    filtered_df = filtered_df[['Local_Timestamp', 'Timestamp', 'Speed (Km/h)', 'Latitude', 'Longitude']]
+
+    return filtered_df
+
+
+def render_corner(filtered_cur_df, filtered_opp_df, left_side_df, right_side_df, zoom, 
+                center_dict, width, height, bearing, size, frames,
+                player_name = "Player", opponent_name = "Opponent",
+                corner = 1):
+    filenames = []
+    images = []
+
+    if not os.path.exists("animation_figures"):
+        os.makedirs("animation_figures")
+    else:
+        for file in os.listdir("animation_figures"):
+            os.remove(os.path.join("animation_figures", file))
+
+    if not os.path.exists("finished_animation_figures"):
+        os.makedirs("finished_animation_figures")
+    else:
+        for file in os.listdir("finished_animation_figures"):
+            os.remove(os.path.join("finished_animation_figures", file))
+
+    if not os.path.exists("animation_charts"):
+        os.makedirs("animation_charts")
+    else: 
+        for file in os.listdir("animation_charts"):
+            os.remove(os.path.join("animation_charts", file))
+
+    x_time_lim = max(filtered_cur_df['Local_Timestamp'].max(), filtered_cur_df['Local_Timestamp'].max())
+
+    images = []
+    with Pool(processes=cpu_count()//2) as pool:
+        args = [(filtered_cur_df, filtered_opp_df, size, player_name, opponent_name, i,
+                        left_side_df, right_side_df, zoom, center_dict, width, height, 
+                        bearing, x_time_lim) for i in range(frames)]
+        results = pool.starmap(create_single_frame, args)
+        
+        for i, image in enumerate(results):
+            if image:
+                images.append(image)
+
+    # Create a GIF
+    gif_path = f'Gifs/corner_animation_{corner}_{player_name}_{opponent_name}.gif'
+    with imageio.get_writer(gif_path, mode='I', duration=1/30) as writer:  # 20 frames per second
+        for filename in images:
+            image = imageio.imread(filename)
+            writer.append_data(image)
+
+    return gif_path
+
+
+if __name__ == "__main__":
+    
+
+    ### NOTE: This entire script was depricated, as it was taking too long to create in real time. 
+    # The code will create a gif of the animation, combining in with streamlit to create a real time animation
+    # Is not phesible in its current state, with the current packages.
+    # Check out the Animation in "GitHub\RacingTrajectories\streamlit\Gifs\corner_animation_1.gif"
+
+    players = ["Ana", "Emil", "Bot"]
+    best_lap = [36, 51, 7]
+
+    start_time = time.time()
+    print("Creating Animation starts now")
+
+    for player_1 in players:
+        for player_2 in players:
+            if player_1 != player_2:
+                print(f"Creating animation for {player_1} vs {player_2}")
+                
+                laps_df, lap_times = prepare_laps_data(name=player_1)
+                cur_lap_df = get_specific_lap(laps_df, lap_number=best_lap[players.index(player_1)]) 
+
+                opp_laps_df, opp_lap_times = prepare_laps_data(name=player_2)
+                opp_cur_lap_df = get_specific_lap(opp_laps_df, lap_number=best_lap[players.index(player_2)])
+
+                left_side_df, right_side_df = load_track_data()
+                left_side_df = transform_coordinates(left_side_df)
+                right_side_df = transform_coordinates(right_side_df)
+
+                ### Corner 1 Gif Creation
+                corner = 1
+                min_timestamp = 0
+                max_timestamp = 40
+                max_lat = 50.3345 # north most point
+                min_lat = 50.3309 # south most point
+                max_lon = 6.94441 # east most point
+                min_lon = 6.93755   # west most point
+                zoom = 17
+
+                filtered_cur_df = create_data_subset(cur_lap_df , min_timestamp, max_timestamp, min_lat, max_lat, min_lon, max_lon)
+                filtered_opp_df = create_data_subset(opp_cur_lap_df , min_timestamp, max_timestamp, min_lat, max_lat, min_lon, max_lon)
+
+                
+
+                filtered_cur_df = render_corner(filtered_cur_df, filtered_opp_df, left_side_df=left_side_df, right_side_df=right_side_df,
+                                                zoom=zoom, center_dict={"Lat":50.3326 , "Lon":6.9405}, width=1400, height=800, bearing=-10, size=4,
+                                                frames= 800 , player_name=player_1, opponent_name=player_2, corner = corner)
+
+                print("\n")
+                print("Current runtime:  {} minutes  {} seconds".format((time.time() - start_time)//60, (time.time() - start_time)%60))
+                print("\n")
+
+                ### Corner 2 Gif Creation
+                corner = 2
+                min_timestamp = 20
+                max_timestamp = 80
+                # Latitude is The Y-axis (More is North (up), Less is South (down))
+                # Longitude is The X-axis (More is East (right), Less is West (left))
+                max_lat = 50.33 # north most point
+                min_lat = 50.32 # south most point
+                max_lon = 6.95 # east most point
+                min_lon = 6.93   # west most point
+                center_dict = {"Lat":50.32636 , "Lon":6.9372}
+                zoom = 16.5
+                bearing = -40
+
+                filtered_cur_df = create_data_subset(cur_lap_df , min_timestamp, max_timestamp, min_lat, max_lat, min_lon, max_lon)
+                filtered_opp_df = create_data_subset(opp_cur_lap_df , min_timestamp, max_timestamp, min_lat, max_lat, min_lon, max_lon)
+
+                start_time = time.time()
+
+                filtered_cur_df = render_corner(filtered_cur_df, filtered_opp_df, left_side_df=left_side_df, right_side_df=right_side_df,
+                                                zoom=zoom, center_dict=center_dict, width=1400, height=800, bearing=bearing, size=4,
+                                                frames= 800 , player_name=player_1, opponent_name=player_2, corner = corner)
+
+                print("\n")
+                print("Current runtime:  {} minutes  {} seconds".format((time.time() - start_time)//60, (time.time() - start_time)%60))
+                print("\n")
+
+                ### Corner 3 Gif Creation
+                corner = 3
+                min_timestamp = 50
+                max_timestamp = 200
+                # Latitude is The Y-axis (More is North (up), Less is South (down))
+                # Longitude is The X-axis (More is East (right), Less is West (left))
+                max_lat = 50.34 # north most point
+                min_lat = 50.335 # south most point
+                max_lon = 6.96 # east most point
+                min_lon = 6.9437   # west most point, 
+                center_dict = {"Lat":50.33661654164608 , "Lon":6.947228193315168}
+                zoom = 17.1
+                bearing = -45
+
+                filtered_cur_df = create_data_subset(cur_lap_df , min_timestamp, max_timestamp, min_lat, max_lat, min_lon, max_lon)
+                filtered_opp_df = create_data_subset(opp_cur_lap_df , min_timestamp, max_timestamp, min_lat, max_lat, min_lon, max_lon)
+
+                start_time = time.time()
+
+                filtered_cur_df = render_corner(filtered_cur_df, filtered_opp_df, left_side_df=left_side_df, right_side_df=right_side_df,
+                                                zoom=zoom, center_dict=center_dict, width=1400, height=800, bearing=bearing, size=4,
+                                                frames= 800 , player_name=player_1, opponent_name=player_2, corner = corner)
+                print("\n")
+                print("Current runtime:  {} minutes  {} seconds".format((time.time() - start_time)//60, (time.time() - start_time)%60))
+                print("\n")
+                
+    print("Creating all Animation finished in  {} minutes  {} seconds".format((time.time() - start_time)//60, (time.time() - start_time)%60))
+                
+
